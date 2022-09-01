382fc090d988c5a2a94afce272a614dde6e9789f
Andres Felipe Ramirez <87092857+f3lip3marin@users.noreply.github.com>
2022-09-01 17:11:37 -0500

    Menu Hamburguesa

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu= document.querySelector('.desktop-menu');
+const desktopMenu= document.querySelector('.desktop-menu')
+//
+const burguerMenu= document.querySelector('.menu');
+const mobileMenu= document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burguerMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu () {
   //llamando a la clase con (classList, y con el toggle la coloreamos)
@@ -10,4 +14,9 @@ function toggleDesktopMenu () {
 
   desktopMenu.classList.toggle("inactive");
 }
- //lo que hicimos aca, y dejo esta nota para mi, fue agragar una seccion de la clase 7 al index principal, para que una vez los usuarios den click en la esquina superior derecha les aparezcan sus ordenes... hicimos esto con la ayuda del DOM, cree dos constantes llamando asus clases, cree el escuchar de enventos y por ultimo una funcion que me muestre y me lo quite cuando este o no.
+ //lo que hicimos aca, y dejo esta nota para mi, fue agragar una seccion de la clase 7 al index principal, para que una vez los usuarios den click en la esquina superior derecha les aparezcan sus ordenes... hicimos esto con la ayuda del DOM, cree dos constantes llamando asus clases, cree el escuchar de enventos y por ultimo una funcion que me muestre y me lo quite cuando este o 
+
+
+ function toggleMobileMenu () {
+     mobileMenu.classList.toggle("inactive");
+ }
