8e96a4a9d73de642585a1889a3d965bcb683a948
Andres Felipe Ramirez <87092857+f3lip3marin@users.noreply.github.com>
2022-09-01 16:38:07 -0500

    Uniendo Secciones
    Se agregó una sección el index principal con una mini ventana de "compras" para que una vez los usuarios den click en la esquina superior derecha les aparezcan sus ordenes. Se creó dos constantes llamando a sus clases, cree el escuchar de eventos y por ultimo una función que me muestre y me lo quite cuando este o no.

---
+++ main.js
@@ -0,0 +1,13 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu= document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+function toggleDesktopMenu () {
+  //llamando a la clase con (classList, y con el toggle la coloreamos)
+  //la quita a poner las clases
+
+
+  desktopMenu.classList.toggle("inactive");
+}
+ //lo que hicimos aca, y dejo esta nota para mi, fue agragar una seccion de la clase 7 al index principal, para que una vez los usuarios den click en la esquina superior derecha les aparezcan sus ordenes... hicimos esto con la ayuda del DOM, cree dos constantes llamando asus clases, cree el escuchar de enventos y por ultimo una funcion que me muestre y me lo quite cuando este o no.
