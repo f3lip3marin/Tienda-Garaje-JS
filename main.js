8f3fd3ed78f533c3144e7c8b64201f2c6c15afd0
Andres Felipe Ramirez <87092857+f3lip3marin@users.noreply.github.com>
2022-09-02 10:37:24 -0500

    Menu Carrito

--- main.js
+++ main.js
@@ -4,12 +4,27 @@ const desktopMenu= document.querySelector('.desktop-menu')
 const burguerMenu= document.querySelector('.menu');
 const mobileMenu= document.querySelector('.mobile-menu')
 
+//
+const menuCarritoIcon= document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector(".product-detail")
+
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 burguerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleeCarritoMenu);
+
 
 function toggleDesktopMenu () {
   //llamando a la clase con (classList, y con el toggle la coloreamos)
   //la quita a poner las clases
+
+
+
+          const isAsideClosed = aside.classList.contains("inactive")
+
+           if(!isAsideClosed){
+          aside.classList.add("inactive")
+        }
 
 
   desktopMenu.classList.toggle("inactive");
@@ -18,5 +33,25 @@ function toggleDesktopMenu () {
 
 
  function toggleMobileMenu () {
+
+          const isAsideClosed = aside.classList.contains("inactive")
+
+           if(!isAsideClosed){
+          aside.classList.add("inactive")
+        }
+
+
      mobileMenu.classList.toggle("inactive");
+ }
+
+ function toggleeCarritoMenu (){
+      const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+
+
+
+        if(!isMobileMenuClosed){
+          mobileMenu.classList.add("inactive")
+        }
+
+    aside.classList.toggle("inactive");
  }
