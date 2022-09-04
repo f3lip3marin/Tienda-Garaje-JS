59c2ac6814d9749c06352c94ab9c44db3d13550b
Andres Felipe Ramirez <87092857+f3lip3marin@users.noreply.github.com>
2022-09-03 20:52:38 -0500

    Cards

--- main.js
+++ main.js
@@ -1,57 +1,98 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu= document.querySelector('.desktop-menu')
-//
-const burguerMenu= document.querySelector('.menu');
-const mobileMenu= document.querySelector('.mobile-menu')
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
-//
-const menuCarritoIcon= document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector(".product-detail")
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
+function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
-burguerMenu.addEventListener("click", toggleMobileMenu);
-menuCarritoIcon.addEventListener("click", toggleeCarritoMenu);
-
-
-function toggleDesktopMenu () {
-  //llamando a la clase con (classList, y con el toggle la coloreamos)
-  //la quita a poner las clases
-
-
-
-          const isAsideClosed = aside.classList.contains("inactive")
-
-           if(!isAsideClosed){
-          aside.classList.add("inactive")
-        }
-
-
-  desktopMenu.classList.toggle("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
 }
- //lo que hicimos aca, y dejo esta nota para mi, fue agragar una seccion de la clase 7 al index principal, para que una vez los usuarios den click en la esquina superior derecha les aparezcan sus ordenes... hicimos esto con la ayuda del DOM, cree dos constantes llamando asus clases, cree el escuchar de enventos y por ultimo una funcion que me muestre y me lo quite cuando este o 
 
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
 
- function toggleMobileMenu () {
+  if (!isAsideClosed) {
+    aside.classList.add('inactive'); 
+  }
+  
+  mobileMenu.classList.toggle('inactive');
+}
 
-          const isAsideClosed = aside.classList.contains("inactive")
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive'); 
+  }
+  
+  aside.classList.toggle('inactive');
+}
 
-           if(!isAsideClosed){
-          aside.classList.add("inactive")
-        }
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Compu',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
 
-     mobileMenu.classList.toggle("inactive");
- }
-
- function toggleeCarritoMenu (){
-      const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
-
-
-
-        if(!isMobileMenuClosed){
-          mobileMenu.classList.add("inactive")
-        }
-
-    aside.classList.toggle("inactive");
- }
+renderProducts(productList);
